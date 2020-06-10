79a6f5ff8cd27c00fa56c1dcad3f858b41378e46
Clayton Guimaraes Leite Pereira <claytonpereira@mtprev.local>
2020-06-10 09:48:37 -0400

    Salvando as alterações na listagem de fichas

--- fichas.js
+++ fichas.js
@@ -9,20 +9,18 @@ request.onload = function() {
     table_line = document.createElement('tr');
     table.appendChild(table_line);
     table_line.appendChild(document.createElement('th')).innerHTML='Ficha Num.';
-    table_line.appendChild(document.createElement('th')).innerHTML='Num. Estante';
-    table_line.appendChild(document.createElement('th')).innerHTML='Num. Prateleira';
-    table_line.appendChild(document.createElement('th')).innerHTML='codigo';
-    table_line.appendChild(document.createElement('th')).innerHTML='Num. Estante';
-    table_line.appendChild(document.createElement('th')).innerHTML='Num. Prateleira';
-    table_line.appendChild(document.createElement('th')).innerHTML='codigo';
-    table_line.appendChild(document.createElement('th')).innerHTML='Num. Estante';
-    table_line.appendChild(document.createElement('th')).innerHTML='Num. Prateleira';
-    table_line.appendChild(document.createElement('th')).innerHTML='codigo';
-    table_line.appendChild(document.createElement('th')).innerHTML='Num. Estante';
-    table_line.appendChild(document.createElement('th')).innerHTML='Num. Prateleira';
-    table_line.appendChild(document.createElement('th')).innerHTML='Num. Estante';
-    table_line.appendChild(document.createElement('th')).innerHTML='Num. Prateleira';
-     data.forEach(element => {
+    table_line.appendChild(document.createElement('th')).innerHTML='Matric. Servidor';
+    table_line.appendChild(document.createElement('th')).innerHTML='Nome Servidor';
+    table_line.appendChild(document.createElement('th')).innerHTML='Nome Mãe';
+    table_line.appendChild(document.createElement('th')).innerHTML='Data Nascimento';
+    table_line.appendChild(document.createElement('th')).innerHTML='CPF';
+    table_line.appendChild(document.createElement('th')).innerHTML='Cod.LOCAL';
+    table_line.appendChild(document.createElement('th')).innerHTML='Estante';
+    table_line.appendChild(document.createElement('th')).innerHTML='Prateleira';
+    table_line.appendChild(document.createElement('th')).innerHTML='RG';
+    table_line.appendChild(document.createElement('th')).innerHTML='Orgão Expedidor';
+    table_line.appendChild(document.createElement('th')).innerHTML='UF';
+    data.forEach(element => {
          table_lineBody = document.createElement('tr');
          table_Data = document.createElement('td');
          table_Data2 = document.createElement('td');
@@ -36,28 +34,37 @@ request.onload = function() {
          table_Data10 = document.createElement('td');
          table_Data11 = document.createElement('td');
          table_Data12 = document.createElement('td');
-         table_Data13 = document.createElement('td');
-         table_Data14 = document.createElement('td');
+    /*      table_Data13 = document.createElement('td');
+         table_Data14 = document.createElement('td'); */
 
          table_Data.innerHTML = element.NUMFICHA;
-         table_Data2.innerHTML = element.NUMESTANTE;
-         table_Data3.innerHTML = element.NUMPRATELEIRA;
-         table_Data4.innerHTML = element.NUMESTANTE;
-         table_Data5.innerHTML = element.NUMPRATELEIRA;
-         table_Data6.innerHTML = element.NUMESTANTE;
-         table_Data7.innerHTML = element.NUMPRATELEIRA;
-         table_Data8.innerHTML = element.NUMESTANTE;
-         table_Data9.innerHTML = element.NUMPRATELEIRA;
-         table_Data10.innerHTML = element.NUMESTANTE;
-         table_Data11.innerHTML = element.NUMPRATELEIRA;
-         table_Data12.innerHTML = element.NUMESTANTE;
-         table_Data13.innerHTML = element.NUMPRATELEIRA;
-         table_Data13.innerHTML = element.NUMPRATELEIRA;
+         table_Data2.innerHTML = element.MATRICULA;
+         table_Data3.innerHTML = element.NOMESERVIDOR;
+         table_Data4.innerHTML = element.NOMEMAE;
+         table_Data5.innerHTML = element.DTNASC;
+         table_Data6.innerHTML = element.CPF;
+         table_Data7.innerHTML = element.CODLOCAL;
+         table_Data8.innerHTML = element.ESTANTE;
+         table_Data9.innerHTML = element.PRATELEIRA;
+         table_Data10.innerHTML = element.RG;
+         table_Data11.innerHTML = element.ORGAOEXP;
+         table_Data12.innerHTML = element.UF;
+         /* table_Data13.innerHTML = element.NUMPRATELEIRA;
+         table_Data13.innerHTML = element.NUMPRATELEIRA; */
 
          
          table_lineBody.appendChild(table_Data)
          table_lineBody.appendChild(table_Data2);
          table_lineBody.appendChild(table_Data3);
+         table_lineBody.appendChild(table_Data4);
+         table_lineBody.appendChild(table_Data5);
+         table_lineBody.appendChild(table_Data6);
+         table_lineBody.appendChild(table_Data7);
+         table_lineBody.appendChild(table_Data8);
+         table_lineBody.appendChild(table_Data9);
+         table_lineBody.appendChild(table_Data10);
+         table_lineBody.appendChild(table_Data11);
+         table_lineBody.appendChild(table_Data12);
          table.appendChild(table_lineBody)
       
        
