60e9fb3276a937b93895a02f519f2f2e9c66955a
Clayton Guimaraes Leite Pereira <claytonpereira@mtprev.local>
2020-06-19 10:30:34 -0400

    Salvando alterações na tela de cadastro

---
+++ cadastrarficha.js
@@ -0,0 +1,21 @@
+const codlocalInput = document.createElement('input');
+const sitfichaInput = document.createElement('input');
+const codusuempInput = document.createElement('input');
+const rootDiv = document.getElementById('root');
+
+codlocalInput.setAttribute('name','CODLOCAL');
+codlocalInput.setAttribute('value','1');
+codlocalInput.setAttribute('class','hidden');
+
+sitfichaInput.setAttribute('name','SITFICHA');
+sitfichaInput.setAttribute('value','1');
+sitfichaInput.setAttribute('class','hidden');
+
+codusuempInput.setAttribute('name','CODUSUEMP');
+codusuempInput.setAttribute('value','1');
+codusuempInput.setAttribute('class','hidden');
+
+
+rootDiv.appendChild(codlocalInput);
+rootDiv.appendChild(sitfichaInput);
+rootDiv.appendChild(codusuempInput);
