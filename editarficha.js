2dcaeb40838236a1a82c49b414ba6c8e84126762
Clayton Guimaraes Leite Pereira <claytonpereira@mtprev.local>
2020-06-26 09:49:24 -0400

    Salvando alterações na tela de edição de fichas

---
+++ editarficha.js
@@ -0,0 +1,56 @@
+
+const codlocalInput = document.createElement('input');
+const sitfichaInput = document.createElement('input');
+const codusuempInput = document.createElement('input');
+const rootDiv = document.getElementById('root');
+
+codlocalInput.setAttribute('name','CODLOCAL');
+codlocalInput.setAttribute('value','1');
+codlocalInput.setAttribute('class','hidden');
+
+sitfichaInput.setAttribute('name','SITFICHA');
+sitfichaInput.setAttribute('value','1');
+sitfichaInput.setAttribute('class','hidden');
+
+codusuempInput.setAttribute('name','CODUSUEMP');
+codusuempInput.setAttribute('value','1');
+codusuempInput.setAttribute('class','hidden');
+
+
+rootDiv.appendChild(codlocalInput);
+rootDiv.appendChild(sitfichaInput);
+rootDiv.appendChild(codusuempInput);
+
+const form = document.getElementById('Cadastrar_ficha');
+
+//take a multi-part form data type and transform in x-www-url-form-encode
+function urlencodeFormData(fd){
+    var s = '';
+    function encode(s){ return encodeURIComponent(s).replace(/%20/g,'+'); }
+    for(var pair of fd.entries()){
+        if(typeof pair[1]=='string'){
+            s += (s?'&':'') + encode(pair[0])+'='+encode(pair[1]);
+        }
+    }
+    return s;
+}
+
+async function enviaDados(formData) {
+let response = await fetch('http://localhost:3000/alterar/fichas/1', {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: formData
+  });
+  
+  let result = await response;
+  alert(result.message);
+}
+
+form.onsubmit = function(event) {
+    const formData = urlencodeFormData(new FormData(form)); 
+  enviaDados(formData);
+  event.preventDefault();
+  alert('submited!');
+}
