e52e626e0e26fafa599586de31badc78f585315c
clayton Pereira <cpereiramt@gmail.com>
2020-07-13 01:16:58 -0300

    Salvando formatação da tela de cadastro de fichas

--- cadastrarficha.js
+++ cadastrarficha.js
@@ -22,6 +22,10 @@ rootDiv.appendChild(sitfichaInput);
 rootDiv.appendChild(codusuempInput);
 
 const form = document.getElementById('Cadastrar_ficha');
+window.onload = function() {
+  $('#date').mask('00/00/0000');
+  $('#cpf').mask('000.000.000-00');
+}
 
 //take a multi-part form data type and transform in x-www-url-form-encode
 function urlencodeFormData(fd){
@@ -49,8 +53,16 @@ let response = await fetch('http://localhost:3000/inserir/ficha', {
 }
 
 form.onsubmit = function(event) {
+    var str = document.getElementsByName('CPF')[0].value;
+    clearCPF = str.replace(/[^\d]+/g,'');
+    document.getElementsByName('CPF')[0].value = clearCPF;
+    const date = document.getElementsByName('DTNASC')[0].value.split('/');
+    const dia = date[0];
+    const mes = date[1];
+    const ano = date[2];
+    const date_Mysql_format = `${ano}/${mes}/${dia}`;
+    document.getElementsByName('DTNASC')[0].value = date_Mysql_format;
     const formData = urlencodeFormData(new FormData(form)); 
   enviaDados(formData);
   event.preventDefault();
-  alert('submited!');
 }
